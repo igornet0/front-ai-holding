6c9e3669584b7f8a95e67164211e928b34ccb49f
igornet0 <892130836@mail.ru>
2025-06-26 15:50:51 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   
+    port: 3000, 
+    strictPort: true, 
+  },
+  
+  build: {
+    rollupOptions: {
+      output: {
+        // Хеширование файлов для кэширования
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
 })
